848ee5763fdeb5f3077f1f8da574279250afd51b
AnushaAnnagiri <64127028+AnushaAnnagiri@users.noreply.github.com>
2020-09-20 22:22:59 +0530

    Add files via upload

---
+++ week-4/SpeakHello.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+(function (window) {
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
+
